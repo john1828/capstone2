af9ecea236b1840047be6a130c8af9a97dc2cdfa
arnelsinadhan19 <arnelsinadhan19@gmail.com>
2024-07-15 12:24:55 +0800

    refactore product controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,136 +1,138 @@
-const Products = require("../models/Product.js");
-const Cart = require("../models/Cart.js");
+const Product = require("../models/Product.js");
 const { errorHandler } = require("../auth");
 
-//[SECTION] Create a product
-module.exports.addProduct = (req, res) => {
-  let newProduct = new Product({
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-  });
-  Products.findOne({ name: req.body.name })
-    .then((existingProduct) => {
-      if (existingProduct) {
-        return res.status(409).send({ message: "Product is already exist" });
-      } else {
-        return newProduct
-          .save()
-          .then((savedProduct) =>
-            res.send({
-              product: newProduct,
-            })
-          )
-          .catch((err) => errorHandler(err, req, res));
-      }
-    })
-    .catch((err) => errorHandler(err, req, res));
-};
-
-//[SECTION] Retrieve all products
-module.exports.getAllProduct = async (req, res) => {
+// Create a product
+module.exports.addProduct = async (req, res) => {
   try {
-    const products = await Products.find({});
-    if (products.length > 0) {
-      return res.status(200).send({ products });
-    } else {
-      return res.status(404).send({ message: "No products found" });
+    const { name, description, price } = req.body;
+
+    // Check if product with the same name already exists
+    const existingProduct = await Product.findOne({ name });
+    if (existingProduct) {
+      return res.status(409).send({ message: "Product already exists" });
     }
+
+    // Create new product instance
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+    });
+
+    // Save product to database
+    const savedProduct = await newProduct.save();
+
+    res.status(201).send({ product: savedProduct });
   } catch (error) {
-    return errorHandler(error, req, res);
+    errorHandler(error, req, res);
   }
 };
 
-//[SECTION] Retrieve all active products
-module.exports.getActiveProduct = (req, res) => {
-  Products.find({ isActive: true })
-    .then((result) => {
-      if (result.length > 0) {
-        return res.status(200).send({ products: result });
-      } else {
-        return res.status(404).send({ message: "No active product found" });
-      }
-    })
-    .catch((error) => errorHandler(error, req, res));
+// Retrieve all products
+module.exports.getAllProduct = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    if (products.length > 0) {
+      res.status(200).send({ products });
+    } else {
+      res.status(404).send({ message: "No products found" });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 };
 
-// Controller function for retrieving a single product
-module.exports.retrieveSingleProduct = (req, res) => {
-  Products.findById(req.params.productId)
-    .then((retrievedProduct) => {
-      if (retrievedProduct) {
-        return res.status(200).send({
-          product: retrievedProduct,
-        });
-      } else {
-        return res.status(404).send({ error: "Product not found" });
-      }
-    })
-    .catch((error) => errorHandler(error, req, res));
+// Retrieve all active products
+module.exports.getActiveProduct = async (req, res) => {
+  try {
+    const activeProducts = await Product.find({ isActive: true });
+    if (activeProducts.length > 0) {
+      res.status(200).send({ products: activeProducts });
+    } else {
+      res.status(404).send({ message: "No active products found" });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 };
 
-// Controller function for updating a product information
-module.exports.updateProduct = (req, res) => {
-  let UpdatedProduct = {
-    name: req.body.name,
-    description: req.body.description,
-    price: req.body.price,
-  };
-
-  Products.findByIdAndUpdate(req.params.productId, UpdatedProduct)
-    .then((product) => {
-      if (product) {
-        return res.status(200).send({
-          message: "Product updated successfully",
-          updatedProduct: product,
-        });
-      } else {
-        return res.status(404).send({
-          error: "Product not found",
-        });
-      }
-    })
-    .catch((error) => errorHandler(error, req, res));
+// Retrieve a single product by ID
+module.exports.retrieveSingleProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const product = await Product.findById(productId);
+    if (product) {
+      res.status(200).send({ product });
+    } else {
+      res.status(404).send({ message: "Product not found" });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 };
 
-// Controller function for archiving a product
-module.exports.archiveProduct = (req, res) => {
-  Products.findByIdAndUpdate(
-    req.params.productId,
-    { isActive: false },
-    { new: true }
-  )
-    .then((product) => {
-      if (product) {
-        return res.status(200).send({
-          message: "Product archive successfully",
-          archiveProduct: product,
-        });
-      } else {
-        return res.status(404).send({ error: "Product not found" });
-      }
-    })
-    .catch((error) => errorHandler(error, req, res));
+// Update a product by ID
+module.exports.updateProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const { name, description, price } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { name, description, price },
+      { new: true }
+    );
+    if (updatedProduct) {
+      res
+        .status(200)
+        .send({ message: "Product updated successfully", updatedProduct });
+    } else {
+      res.status(404).send({ message: "Product not found" });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 };
 
-// Controller function for activating a product
-module.exports.activateProduct = (req, res) => {
-  Products.findByIdAndUpdate(
-    req.params.productId,
-    { isActive: true },
-    { new: true }
-  )
-    .then((product) => {
-      if (product) {
-        return res.status(200).send({
-          message: "Product activated successfully",
-          activateProduct: product,
-        });
-      } else {
-        return res.status(404).send({ error: "Product not found" });
-      }
-    })
-    .catch((error) => errorHandler(error, req, res));
+// Archive a product by ID
+module.exports.archiveProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const archivedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { isActive: false },
+      { new: true }
+    );
+    if (archivedProduct) {
+      res
+        .status(200)
+        .send({ message: "Product archived successfully", archivedProduct });
+    } else {
+      res.status(404).send({ message: "Product not found" });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+};
+
+// Activate a product by ID
+module.exports.activateProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const activatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      { isActive: true },
+      { new: true }
+    );
+    if (activatedProduct) {
+      res
+        .status(200)
+        .send({ message: "Product activated successfully", activatedProduct });
+    } else {
+      res.status(404).send({ message: "Product not found" });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
 };
 
 // Search product by name
@@ -138,42 +140,32 @@ module.exports.searchByName = async (req, res) => {
   try {
     const { name } = req.body;
     if (!name) {
-      return res.status(400).json({ error: "Product name is required" });
+      return res.status(400).send({ error: "Product name is required" });
     }
-
-    const products = await Products.find({ name });
-
+    const products = await Product.find({ name });
     if (products.length === 0) {
-      return res.status(404).json({ error: "No product found" });
+      return res.status(404).send({ message: "No product found" });
     }
-
-    res.status(200).json(products);
+    res.status(200).send({ products });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while searching for products" });
+    errorHandler(error, req, res);
   }
 };
 
 // Search product by price range
 module.exports.searchByPrice = async (req, res) => {
-  const { minPrice, maxPrice } = req.body;
-
-  if (minPrice === undefined || maxPrice === undefined) {
-    return res
-      .status(400)
-      .json({ error: "Minimum Price and Max Price are required" });
-  }
-
   try {
-    const products = await Products.find({
+    const { minPrice, maxPrice } = req.body;
+    if (minPrice === undefined || maxPrice === undefined) {
+      return res
+        .status(400)
+        .send({ error: "Minimum Price and Max Price are required" });
+    }
+    const products = await Product.find({
       price: { $gte: minPrice, $lte: maxPrice },
     });
-
-    res.json(products);
+    res.status(200).send({ products });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while searching for products" });
+    errorHandler(error, req, res);
   }
 };
