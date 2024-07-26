62f9e39f6cd0c462c28ac8ff76459e83cf815eaa
Arnel De leon Sinadhan <arnelsinadhan19@gmail.com>
2024-07-26 06:34:50 +0000

    Update 'index.js'

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ mongoose.connection.once("open", () =>
   console.log("Now connected to MongoDB Atlas")
 );
 // static files form the uploads directory
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/b4/uploads", express.static(path.join(__dirname, "uploads")));
 // User routes
 app.use("/b4/users", userRoutes);
 // Product routes
