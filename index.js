e1e91919269e5fbeac61fbd46e82d22e7ac761a4
arnelsinadhan19 <arnelsinadhan19@gmail.com>
2024-07-29 13:03:30 +0800

    removed forward slashes

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ extended: true }));
 // CORS options
 const corsOptions = {
   origin: [
-    "https://capstone3-wine.vercel.app/",
-    "https://capstone3-git-master-arnels-projects-6434d9a7.vercel.app/",
-    "https://capstone3-o9z28p3jy-arnels-projects-6434d9a7.vercel.app/",
+    "https://capstone3-wine.vercel.app",
+    "https://capstone3-git-master-arnels-projects-6434d9a7.vercel.app",
+    "https://capstone3-o9z28p3jy-arnels-projects-6434d9a7.vercel.app",
   ],
   credentials: true,
   optionSuccessStatus: 200,
